11aeaedbc9215ca52a24251b4aae331a62997b69
Samuel Chan <cchin.samuel@gmail.com>
2022-05-07 12:55:57 +0800

    feat: add firebase settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:dating_app/features/splash/splash_page.dart';
+import 'package:dating_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
